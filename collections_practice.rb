2068cdd8a72a7b3a53d6d1b0d3299582810644db
Yosef Lehrer <yolehrer@gmail.com>
2019-06-25 13:00:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,7 +47,7 @@ def find_cool(array)
   new_array = []
   array.each do |hash|
     hash.each do |key, value|
-      if key == "black"
+      if key == "blake"
         new_array << key
         new_array << value
       end
