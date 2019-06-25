8a325c193acc5fe5072a703d6dfd8c0a65fbe55f
Yosef Lehrer <yolehrer@gmail.com>
2019-06-25 12:54:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,7 +47,8 @@ def find_cool(array)
   new_array = []
   array.each do |hash|
     hash.each do |key, value|
-      if value == "cool"
+      if value == "sort of cool"
+      else
         new_array << hash
       end
    end
